e2ec7f05f9fe9c7ff7f1770d3b885e10e118f6ea
Macaco2593 <santiagopjq13@gmail.com>
2022-01-29 17:58:42 -0400

    Primer Cambio

---
+++ app.js
@@ -0,0 +1,12 @@
+const input1 = document.getElementById("input1")
+const input2 = document.getElementById("input2")
+const input3 = document.getElementById("input3")
+
+console.log(input2)
+input2.addEventListener("click", (num1, num2) => (num1 + num2))
+
+
+input3.addEventListener("click", (num1, num2) =>{
+    Number 
+    return num1 - num2
+})
