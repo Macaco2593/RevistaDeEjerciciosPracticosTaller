cd0a552fafabd3773446dc1a9e87c6a4f014fb67
Santiago <santiagopjq13@gmail.com>
2022-01-31 18:36:13 -0400

    segundo cambio

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const input1 = document.getElementById("input1")
-const input2 = document.getElementById("input2")
-const input3 = document.getElementById("input3")
-
-console.log(input2)
-input2.addEventListener("click", (num1, num2) => (num1 + num2))
-
-
-input3.addEventListener("click", (num1, num2) =>{
-    Number 
-    return num1 - num2
+const input1 = document.getElementById("input1").value
+const input2 = document.getElementById("input2").value
+input2.addEventListener("keyup", (event) =>{
+     let input2 = event.path(0).value
 })
+//const input3 = document.getElementById("input3").value
+
+console.log(input1)
+
+// input3.addEventListener("click", (num1, num2) =>{
+//     Number 
+//     return num1 - num2
+// })
+
